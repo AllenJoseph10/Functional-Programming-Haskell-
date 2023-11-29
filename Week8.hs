1102665db669eda2d17f7c702cee68ab9c48fc2d
Allen Jen Joseph <118963252+AllenJoseph10@users.noreply.github.com>
2023-11-29 14:05:54 +0000

    Update Week8.hs

--- Week8.hs
+++ Week8.hs
@@ -1,3 +1,133 @@
+isBST :: Ord a => BT a -> Bool
+isBST Empty        = True
+isBST (Fork x l r) = allSmaller x l
+                  && allBigger  x r
+                  && isBST l
+                  && isBST r
+
+allSmaller :: Ord a => a -> BT a -> Bool
+allSmaller x Empty        = True
+allSmaller x (Fork y l r) = y < x
+                         && allSmaller x l
+                         && allSmaller x r
+
+allBigger :: Ord a => a -> BT a -> Bool
+allBigger x Empty = True
+allBigger x (Fork y l r) = y > x
+            && allBigger x l
+            && allBigger x r
+
+isBST' :: Ord a => BT a -> Bool
+isBST' t = isIncreasing(treeInOrder t)
+
+isIncreasing :: Ord a => [a] -> Bool
+isIncreasing []       = True
+isIncreasing (x:[])   = True
+isIncreasing (x:y:zs) = x < y && isIncreasing(y:zs)
+
+occurs :: Ord a => a -> BT a -> Bool
+occurs x Empty        = False
+occurs x (Fork y l r) = x == y
+                     || (x < y && occurs x l)
+                     || (x > y && occurs x r)
+
+insert' :: Ord a => a -> BT a -> Maybe(BT a)
+insert' x Empty                   = Just(Fork x Empty Empty)
+insert' x (Fork y l r) | x < y     = case insert' x l of
+                                       Nothing -> Nothing
+                                       Just l' -> Just(Fork y l' r)
+                       | x > y     = case insert' x r of
+                                       Nothing -> Nothing
+                                       Just r' -> Just(Fork y l r')
+                       | otherwise = Nothing
+
+delete :: Ord a => a -> BT a -> BT a
+delete x Empty = Empty -- or you may prefer undefined (and even Nothing)
+delete x (Fork y l r) | x < y                = Fork y (delete x l) r
+                      | x > y                = Fork y l (delete x r)
+                      | x == y && l == Empty = r
+                      | x == y && r == Empty = l
+                      | otherwise            = Fork (largestOf l) (withoutLargest l) r
+
+largestOf :: Ord a => BT a -> a
+largestOf Empty            = undefined
+largestOf (Fork x l Empty) = x
+largestOf (Fork x l r)     = largestOf r
+
+withoutLargest :: Ord a => BT a -> BT a
+withoutLargest Empty            = undefined
+withoutLargest (Fork x l Empty) = l
+withoutLargest (Fork x l r)     = Fork x l (withoutLargest r)
+
+                     
+insert :: Ord a => a -> BT a -> BT a
+insert x Empty                    = Fork x Empty Empty
+insert x (Fork y l r) | x < y     = Fork y (insert x l) r
+                      | x > y     = Fork y l (insert x r)
+                      | otherwise = Fork y l r
+
+randomInts :: [Int]
+randomInts = randomRs (minBound,maxBound) (mkStdGen seed)
+             where seed = 42
+
+inserts :: Ord a => [a] -> BT a -> BT a
+inserts []     t = t
+inserts (x:xs) t = inserts xs (insert x t)
+
+aBigBST :: BT Int
+aBigBST = inserts (take (10^6) randomInts) Empty
+
+itsHeight = height aBigBST
+itsSize   = size aBigBST
+itsBST    = isBST aBigBST
+itsBST'   = isBST' aBigBST
+
+deletes :: Ord a => [a] -> BT a -> BT a
+deletes []     t = t
+deletes (x:xs) t = deletes xs (delete x t)
+
+aSmallerTree :: BT Int
+aSmallerTree = deletes (take (5 * (10^5)) randomInts) aBigBST
+
+evenBigger :: BT Int
+evenBigger = inserts (take (10^7) randomInts) Empty
+
+fullBST :: Integer -> Integer -> BT Integer
+fullBST x y | x == y    = Fork x Empty Empty
+            | x+1 == y  = Fork y (Fork x Empty Empty) Empty
+            | x+1 <  y  = Fork m (fullBST x (m-1)) (fullBST (m+1) y)
+            | otherwise = undefined
+  where m = (x + y) `div` 2
+
+bstsort :: Ord a => [a] -> [a]
+bstsort xs = treeInOrder(inserts xs Empty)
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort [l | l <- xs, l < x]
+            ++ [x]
+            ++ qsort [r | r <- xs, r >= x]
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] [] = []
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+  | x <= y    = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
+
+eosplit :: [a] -> ([a],[a])
+eosplit []       = ([],[])
+eosplit [x]      = ([x],[])
+eosplit (e:o:xs) = case eosplit xs of
+                     (es,os) -> (e:es, o:os)
+
+msort :: Ord a => [a] -> [a]
+msort xs | length xs <= 1  =  xs
+         | otherwise       = merge (msort es) (msort os)
+                             where (es, os) = eosplit xs
+
+
 fibm :: Monad m => Integer -> m Integer
 fibm 0 = pure 0
 fibm 1 = pure 1
