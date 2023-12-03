62cdb0f69d0b5d515766c5b4b58849d47693f2b6
Allen Jen Joseph <118963252+AllenJoseph10@users.noreply.github.com>
2023-12-03 16:29:46 +0000

    Update Week7.hs

--- Week7.hs
+++ Week7.hs
@@ -1,7 +1,21 @@
 -- Binary Trees
 
-Data BT a = Empty | Fork a (BT a) (BT a)
+data BT a = Empty
+          | Fork a (BT a) (BT a) deriving (Show, Read, Eq, Ord)
+
 
 mirror :: BT a -> BT a
 mirror Empty = Empty
-mirror (Fork x l r) = x (mirror r) (mirror l)
+mirror (Fork x l r) = Fork x (mirror r) (mirror l)
+
+size :: BT a -> Integer
+size Empty = 0
+size (Fork x l r) = 1 + (size l) + (size r)
+
+leaves :: BT a -> Integer
+leaves Empty = 0
+leaves (Fork x l r) = leaves l + leaves r
+
+height :: BT a -> Integer
+height Empty = 0
+height (Fork x l r) = 1 + max (height l) (height r)
